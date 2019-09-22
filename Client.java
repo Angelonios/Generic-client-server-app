9fd7caf225408ee84f9363bc9942a45994c5a3e6
Angelonios <angel8893@email.cz>
2019-09-22 08:56:21 +0200

    initial commit

--- Client.java
+++ Client.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Client {
+    private InetAddress serverAddress;
+    private int serverPort;
+
+    public Client(String address, int port) throws UnknownHostException {
+        serverAddress = InetAddress.getByName(address);
+        serverPort = port;
+    }
 }
