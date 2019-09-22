dfe1be0d83e5fafc35ce887c5178dc2de2976c6c
Angelonios <angel8893@email.cz>
2019-09-22 09:55:42 +0200

    added logger and refactored the socket stuff

--- Client.java
+++ Client.java
@@ -1,12 +1,24 @@
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.logging.Logger;
 
 public class Client {
-    private InetAddress serverAddress;
-    private int serverPort;
 
-    public Client(String address, int port) throws UnknownHostException {
-        serverAddress = InetAddress.getByName(address);
-        serverPort = port;
+    private static final Logger logger = Logger.getLogger(Client.class.getName());
+
+    private Socket socket;
+
+
+
+    public Client() throws IOException {
+        connectToServer("localhost", 8888);
     }
+
+    private void connectToServer(String address, int port) throws IOException{
+        socket = new Socket(InetAddress.getByName(address), port);
+    }
+
+
 }
